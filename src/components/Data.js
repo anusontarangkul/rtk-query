a5ffcdfea80e53676bd3f6308dd8b295a51186b4
David Anusontarangkul <anusontarangkul.d@gmail.com>
2023-02-12 18:37:20 -0800

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,9 @@
+import { useGetAllProductsQuery, useGetProductQuery } from "../features/apiSlice"
+
+export const Data = () => {
+  const { data: allProductsData } = useGetAllProductsQuery();
+  const { data: singleProductData } = useGetProductQuery("iphone");
+  console.log(allProductsData)
+  console.log(singleProductData)
+  return <div>Data: </div>
+}
